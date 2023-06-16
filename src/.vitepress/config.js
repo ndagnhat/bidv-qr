94938114896920b76e10e9cfdae002bfc271549d
Sunday <ndn2406@icloud.com>
2023-06-17 01:56:24 +0700

    1

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/qrbidv/",
   title: "BIDV QR",
   description: "BIDV QR",
   themeConfig: {
