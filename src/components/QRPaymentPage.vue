bc8fc77073f912ad705ac7c9405add5dcd55b04f
Sunday <65029229+ndagnhat@users.noreply.github.com>
2023-06-19 09:06:44 +0000

    1

--- src/components/QRPaymentPage.vue
+++ src/components/QRPaymentPage.vue
@@ -12,60 +12,41 @@ var QRNumber = null;
 var QRName = null;
 
 const listBanks = ref([]);
-const bankId = ref('970418');
+const bankCode = ref('BIDV');
 const accountNo = ref('');
 const accountName = ref('');
 const amount = ref('');
 const description = ref('');
-const displayInfor = ref(true)
+const displayInfor = ref(true);
+const frame = ref("noframe");
 
 onMounted(() => {
-    QRFrame = new fabric.Canvas("canvas", {
-        backgroundColor: "#ffffff",
-        width: 1000,
-        height: 1000
-    });
+    QRFrame = new fabric.Canvas("canvas", { backgroundColor: "#ffffff", width: 1000, height: 1000 });
     let qrviewWidth = qrview.value.clientWidth;
     QRFrame.setZoom(qrviewWidth / 1000);
     QRFrame.setWidth(qrviewWidth);
     QRFrame.setHeight(qrviewWidth);
-    fabric.Image.fromURL(withBase("/qrdefault.png"), function (img) {
-        QRCode = img;
-        QRCode.left = 200;
-        QRCode.top = 100;
-        QRCode.scaleX = 600 / QRCode.width;
-        QRCode.scaleY = 600 / QRCode.height;
-        QRFrame.add(QRCode);
-        QRFrame.renderAll();
-    });
-     QRBank = new fabric.Text('', {
-        top: 730,
-        fill: '#006B68'
-    });
-    QRBank.left = 500 - (QRBank.getScaledWidth() /2);
-     QRNumber = new fabric.Text('', {
-        top: 780,
-        fill: '#006B68'
-    });
-    QRNumber.left = 500 - (QRNumber.getScaledWidth() /2);
-    QRName = new fabric.Text('', {
-        top: 820,
-        fill: '#006B68'
-    });
-    QRName.left = 500 - (QRName.getScaledWidth() /2);
+    QRBank = new fabric.Text('', { });
+    QRNumber = new fabric.Text('', { });
+    QRName = new fabric.Text('', { });
     QRFrame.add(QRBank);
     QRFrame.add(QRNumber);
     QRFrame.add(QRName);
-    QRFrame.renderAll();
+    fabric.Image.fromURL(withBase("/qrdefault.png"), function (img) {
+        //img.visible = false;
+        QRCode = img;
+        QRFrame.add(QRCode);
+        QRFrame.renderAll();
+        selectNoFrame();
+    });
 
-
+    // load bank data
     fetch('https://api.vietqr.io/v2/banks')
         .then(response => response.json())
         .then(data => {
             if (data.code == '00') {
                 listBanks.value = data.data;
             }
-            console.log('Error fetching data:', data);
         })
         .catch(error => {
             console.error('Error fetching data:', error);
@@ -73,7 +54,7 @@ onMounted(() => {
 });
 
 function createQRCode() {
-    let qrlink = "https://img.vietqr.io/image/" + bankId.value + "-" + accountNo.value + "-xyzWuLb.png?amount=" + amount.value + "&addInfo=" + description.value + "&accountName=" + accountName.value;
+    let qrlink = "https://img.vietqr.io/image/" + bankCode.value + "-" + accountNo.value + "-xyzWuLb.png?amount=" + amount.value + "&addInfo=" + description.value + "&accountName=" + accountName.value;
     const left = QRCode.left
     const top = QRCode.top
     const width = QRCode.width * QRCode.scaleX;
@@ -88,15 +69,21 @@ function createQRCode() {
     }, {
         crossOrigin: 'anonymous'
     });
+    
+    // const bankName = listBanks.value.find(bank => bank.code = bankCode.value).name;
+    //     QRBank.text = bankName;
+    //     QRFrame.renderAll();
 
-    if(displayInfor.value) {
-        QRBank.text = "abc";
-        QRBank.left = 500 - (QRBank.getScaledWidth() /2);
-        QRFrame.renderAll();
-    }
+    // if(displayInfor.value) {
+
+    //     QRBank.text = "abc";
+    //     QRBank.left = 500 - (QRBank.getScaledWidth() / 2);
+    //     QRFrame.renderAll();
+    // }
 }
 
 function selectNoFrame() {
+    frame.value = "noframe";
     QRFrame.setBackgroundImage(null);
     QRFrame.setBackgroundColor("#ffffff");
 
@@ -104,14 +91,19 @@ function selectNoFrame() {
     QRFrame.setZoom(qrviewWidth / 1000);
     QRFrame.setWidth(qrviewWidth);
     QRFrame.setHeight(qrviewWidth);
+    
+    QRCode.left = 200;
+    QRCode.top = 100;
+    QRCode.scaleX = 600 / QRCode.width;
+    QRCode.scaleY = 600 / QRCode.height;
 
-    if (QRCode != null) {
-        const frameWidth = QRFrame.getWidth() / QRFrame.getZoom();
-        QRCode.left = frameWidth / 6;
-        QRCode.top = frameWidth / 8;
-        QRCode.scaleX = (frameWidth / 3 * 2) / QRCode.width;
-        QRCode.scaleY = (frameWidth / 3 * 2) / QRCode.height;
-    }
+    QRBank.top = 750;
+    QRBank.viewportCenterH();
+    QRNumber.top = 825;
+    QRNumber.viewportCenterH();
+    QRName.top = 900;
+    QRName.viewportCenterH();
+
     QRFrame.renderAll();
 }
 
@@ -123,8 +115,8 @@ function selectNoFrame() {
             <h1>Input Payment Infor</h1>
             <div class="vp-field-group">
                 <label for="selBank" class="vp-field-label">Choose a bank</label>
-                <select id="selBank" v-model="bankId" name="bankId" class="vp-field">
-                    <option v-for="bank in listBanks" :value="bank.bin" :key="bank.bin">
+                <select id="selBank" v-model="bankCode" name="bankCode" class="vp-field">
+                    <option v-for="bank in listBanks" :value="bank.code">
                         {{ bank.name }}
                     </option>
                 </select>
@@ -163,12 +155,12 @@ function selectNoFrame() {
             <div class="container">
                 <h6>Select a frame</h6>
                 <div class="row">
-                    <div class="thumbnail" style="background-color: white; width: 40px;" @click="selectNoFrame" />
-                    <img src="/noframe.png" alt="No Frame" class="thumbnail" />
-                    <img src="/noframe.png" alt="No Frame" class="thumbnail" />
-                    <img src="/noframe.png" alt="No Frame" class="thumbnail" />
-                    <img src="/noframe.png" alt="No Frame" class="thumbnail" />
-                    <img src="/noframe.png" alt="No Frame" class="thumbnail" />
+                    <div class="thumbnail" style="background-color: white; width: 40px;" :class="{ 'active': frame == 'noframe' }" @click="selectNoFrame" />
+                    <img src="/frame1.png" alt="Frame 1" class="thumbnail" :class="{ 'active': frame == 'frame1' }" @click="selectFrame1" />
+                    <img src="/frame2.png" alt="Frame 2" class="thumbnail" :class="{ 'active': frame == 'frame2' }" @click="selectFrame2"/>
+                    <img src="/frame3.png" alt="Frame 3" class="thumbnail" :class="{ 'active': frame == 'frame3' }" @click="selectFrame3"/>
+                    <img src="/frame4.png" alt="Frame 4" class="thumbnail" :class="{ 'active': frame == 'frame4' }" @click="selectFrame4"/>
+                    <img src="/frame5.png" alt="Frame 5" class="thumbnail" :class="{ 'active': frame == 'frame5' }" @click="selectFrame5"/>
                     <input type="file" accept="image/*" @change="onFileChange" title="abc" class="thumbnail">
                 </div>
                 <div class="vp-field-button">
